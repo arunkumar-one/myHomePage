97cd405548d758e9bb228b90f81d798d8550ff0d
Arunkumar <arunkumar.kg@zohocorp.com>
2024-03-13 23:13:26 +0530

    Added stop funciton

--- runScript.sh
+++ runScript.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-start()
+
+stopPort()
 {
+    local PORT=$1
     if [ -f "node_$PORT.pid" ];then
         kill $(cat node_$PORT.pid)
+        rm  node_$PORT.pid
     fi
+}
+
+
+startPort()
+{
+    
+    stopPort $1
+    export PORT=$1
     setsid sudo -E node app.js &
     echo $! > node_$PORT.pid
 }
 
-export PORT=80
-start 
-export PORT=443
-start
+startAll()
+{
+    startPort 80
+    startPort 443
+}
 
+stopAll()
+{
+    stopPort 80
+    stopPort 443
+}
+
+$@
 exit 0
